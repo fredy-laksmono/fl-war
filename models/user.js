8e96f6d74045ce9981f002e4bad7d3f84bd4bf40
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-10-21 09:52:38 -0500

    Add: user initial schema, initial user seed

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const { Schema } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    deck_id: { type: Boolean }
+  },
+  { timestamps: true }
+);
+
+module.exports = userSchema;
