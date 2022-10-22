583c54eb5e2ac6b77d30fd1e16f62b0595caa774
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-10-22 11:32:46 -0500

    Fix; Checking user id valid

--- fl-war-frontend/src/components/Account.js
+++ fl-war-frontend/src/components/Account.js
@@ -3,7 +3,11 @@ import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
 const Account = (props) => {
-  const [playerName, updatePlayerName] = useState();
+  const [playerDataState, updatePlayerData] = useState({
+    name: "",
+    id: "",
+    deck_id: ""
+  });
 
   const { userId } = useParams();
 
@@ -14,24 +18,58 @@ const Account = (props) => {
         return response;
       })
       .catch((error) => {
-        console.log(error);
+        console.log("this is error", error);
       });
-    console.log(userObject);
-    return userObject;
+    console.log("this is userObject after checking", userObject);
+    if (userObject.data._id === id) {
+      console.log("user found");
+      updatePlayerData({
+        name: userObject.data.name,
+        id: userObject.data._id
+      });
+      return userObject;
+    }
   };
 
   let toRender = (
     <div>
-      <h5>Player Id is not detected, please create a new user account</h5>
-      <label>Name: </label>
-      <button></button>
+      <h5>Player Id is not valid, please create a new user account</h5>
+      <Link to="/new">Create account page</Link>
     </div>
   );
-  if (userId) {
-    if (check(userId)) {
-      toRender = <div>Welcome player {userId}</div>;
+
+  const updateRender = () => {
+    console.log("update render called", playerDataState);
+    if (playerDataState.name) {
+      toRender = <div>Welcome player {playerDataState.name}</div>;
+    } else {
+      toRender = (
+        <div>
+          <h5>Player Id is not valid, please create a new user account</h5>
+          <Link to="/new">Create account page</Link>
+        </div>
+      );
     }
+  };
+
+  useEffect(() => {
+    if (userId.length == 24) {
+      check(userId);
+    }
+    updateRender();
+  }, []);
+
+  if (playerDataState.name) {
+    toRender = <div>Welcome player {playerDataState.name}</div>;
+  } else {
+    toRender = (
+      <div>
+        <h5>Player Id is not valid, please create a new user account</h5>
+        <Link to="/new">Create account page</Link>
+      </div>
+    );
   }
+
   return toRender;
 };
 
