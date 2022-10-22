37757fee5a0fb422135ce4da0d5cb52a21681683
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-10-22 17:49:28 -0500

    Add: race schema

---
+++ models/race.js
@@ -0,0 +1,10 @@
+const { Schema } = require("mongoose");
+
+const raceSchema = new Schema(
+  {
+    name: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = raceSchema;
